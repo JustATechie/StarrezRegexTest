c12b0f773962c73f52c77d968e48e04f1238e553
JustATechie <47167717+JustATechie@users.noreply.github.com>
2021-11-15 16:39:31 +0000

    added modified generic phone number exp to be more lenient on country codes.

--- src/Regex.py
+++ src/Regex.py
@@ -2,6 +2,8 @@ import re
 
 # Pattern Strings
 contactNameExp = "^([a-zA-Z]+( (?=[a-zA-Z])|-(?=[a-zA-Z])){0,1})*$"
+phoneNumberExp = "\+{0,1}(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{7,14}$"
 
 # Pattern Objects
 contactNamePattern = re.compile(contactNameExp)
+phoneNumberPattern = re.compile(phoneNumberExp)
